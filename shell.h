2c772c540996eda3d6a9ee6a2df052fe025cfbb5
SANTIAGOH22 <hinestrozasantiago@gmail.com>
2023-04-27 15:40:51 -0500

    Adding shell.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+void prompt(char **av __attribute__((unused)), char **env);
+
+#endif
